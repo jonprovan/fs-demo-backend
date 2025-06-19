7ee1908395582a51335ebb0eae1184a01c30a7e3
Jon Provan <jprovan@skillstorm.com>
2025-06-18 20:12:37 -0400

    Update 4

---
+++ backend/src/main/java/com/skillstorm/controllers/HealthController.java
@@ -0,0 +1,14 @@
+package com.skillstorm.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class HealthController {
+
+	@GetMapping
+	public void healthCheck() {}
+	
+}
